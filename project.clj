613e3acea0b25db972734ffc248999366a0a6c7d
Ethan Fast <ethan@d-172-26-4-113.bootp.virginia.edu>
2010-02-22 13:06:36 -0500

    clojar

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A small framework for writing genetic algorithms"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [leiningen/lein-swank "1.1.0"]
-		 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
