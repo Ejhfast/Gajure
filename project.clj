1930a1eacf57ea70827d21ffe860a67238c7ec86
Ethan Fast <Ejhfast@gmail.com>
2010-12-21 15:28:54 -0500

    update clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject Gajure "1.0.0-SNAPSHOT"
   :description "A small framework for writing genetic algorithms"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [leiningen/lein-swank "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+										 [swank-clojure "1.2.1"]])
